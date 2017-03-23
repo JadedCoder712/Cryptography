f122116612cc8215ebf1655dfb38456d02aa9c5b
JadedCoder712 <thomaspostans@hanovernorwichschools.org>
2017-03-23 14:26:11 -0400

    Updated from Brython Server: 3/23/2017 2:26:10 PM

--- cryptography.py
+++ cryptography.py
@@ -7,6 +7,27 @@ Assignment:
 
 Write and submit a program that encrypts and decrypts user data.
 
+Ideas: I will have three "if" functions for the program, each for the different menu options. Wrong: The "key" will be made the name of a lambda function with general parameters, but which uses a randomly sorted list (using the random function) will put inputs into the lambda function.
+
 See the detailed requirements at https://github.com/HHS-IntroProgramming/Cryptography/blob/master/README.md
 """
 associations = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789 .,:;'\"/\\<>(){}[]-=_+?!"
+
+menu=input("Enter e to encrypt, d to decrypt, or q to quit: ")
+
+if menu is "q":
+    print("Goodbye!")
+
+if menu is "e":
+    message=input("Message: ")
+    key=input("Key: ")
+    
+    
+    
+    
+
+
+if menu is "d":
+    message1=input("Message: ")
+    key1=input("Key: ")
+    
